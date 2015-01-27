eb9d39408d1e7b374f51004226a35c54935258e5
forecho <caizhenghai@gmail.com>
2015-01-27 17:11:15 +0800

    添加版本控制的路由管理

--- config/restful.php
+++ config/restful.php
@@ -1,8 +1,14 @@
 <?php
 return [
- 	'PUT,PATCH users/<id>' => 'user/update',
-    'DELETE users/<id>' => 'user/delete',
-    'GET,HEAD users/<id>' => 'user/view',
-    'POST users' => 'user/create',
-    'GET,HEAD users' => 'user/index',
+    'PUT,PATCH /users/<id>' => '/user/update',
+    'DELETE /users/<id>' => '/user/delete',
+    'GET,HEAD /users/<id>' => '/user/view',
+    'POST /users' => '/user/create',
+    'GET,HEAD /users' => '/user/index',
+
+ 	'PUT,PATCH <module>/users/<id>' => '<module>/user/update',
+    'DELETE <module>/users/<id>' => '<module>/user/delete',
+    'GET,HEAD <module>/users/<id>' => '<module>/user/view',
+    'POST <module>/users' => '<module>/user/create',
+    'GET,HEAD <module>/users' => '<module>/user/index',
 ];
